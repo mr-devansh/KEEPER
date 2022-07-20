a215377f7d5a2b596ccf01a8ab520c929f4fa1db
mr-devansh <devu0996@gmail.com>
2022-07-21 00:43:09 +0530

    V1

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+import Header from "./header";
+import Note from "./note";
+import Footer from "./footer";
+
+function App() {
+ return (
+ <div>
+    <Header/>
+    <Note/>
+    <Footer/>
+ </div> 
+ )  
+}
+export default App;
